f2a0d4a85b4403ea719f67913359b493dcd567d6
Pyneni Chamanthi <54474521+Chamanthi17@users.noreply.github.com>
2025-07-04 17:36:32 -0500

    Update DButil.java

--- DButil.java
+++ DButil.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DButil {
     private static final String URL = "jdbc:mysql://localhost:3306/student_tracker";
     private static final String USER = "root";
-    private static final String PASSWORD = "chamu"; // 
+    private static final String PASSWORD = "your password"; // password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
